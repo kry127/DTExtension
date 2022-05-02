3db64c5c2c3a41b518bc525d7e91828f59ac1399
kry127 <kry127@yandex.ru>
2022-05-02 22:50:48 +0300

    Remove split cursor request -- it is useless, no one would implement it, but protocol will be simpler

--- examples/kotlin_s3_v0_2/connector/build.gradle.kts
+++ examples/kotlin_s3_v0_2/connector/build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("io.grpc:grpc-stub:$grpcVersion")
     implementation("io.grpc:grpc-kotlin-stub:$kotlinGrpcVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("net.pwall.json:json-kotlin-schema:0.34")
+    implementation("com.beust:klaxon:5.5")
     implementation(project(":protobuf"))
 
     testImplementation("junit", "junit", "4.12")
